005c73d0472c2d5614f7fa1f8b6d48ace1c3e016
Zach Florian <zachary.florian@uconn.edu>
2024-07-02 10:31:14 -0400

    added films and planets to character page

--- react/src/components/CharacterPage.jsx
+++ react/src/components/CharacterPage.jsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom';
 function CharacterPage() {
   const { id } = useParams();
   const [character, setCharacter] = useState(null);
+  const [films, setFilms] = useState(null);
+  const [planet, setPlanet] = useState(null);
 
   useEffect(() => {
     // Fetch character data based on the id
@@ -11,26 +13,46 @@ function CharacterPage() {
       .then(response => response.json())
       .then(data => setCharacter(data))
       .catch(error => console.error('Error fetching character:', error));
+
+    fetch(`http://127.0.0.1:3000/api/characters/${id}/films`)
+      .then(response => response.json())
+      .then(data => setFilms(data))
+      .catch(error => console.error('Error fetching films:', error));
+
+    fetch(`http://127.0.0.1:3000/api/planets/${character.homeworld}`)
+      .then(response => response.json())
+      .then(data => setPlanet(data))
+      .catch(error => console.error('Error fetching planet:', error));
   }, [id]);
 
-  if (!character) {
+  if (!character && !films && !planet) {
     return <div>Loading...</div>;
   } else {
     console.log(character)
+    console.log(films)
+    console.log(planet)
   }
 
   return (
+    <>
     <div>
       <h1>{character['0'].name}</h1>
       <p>Height: {character['0'].height}</p> 
       <p>Birth Year: {character['0'].birth_year}</p> 
       <p>Gender: {character['0'].gender}</p> 
       <p>Hair Color: {character['0'].hair_color}</p> 
-      <p>Homeworld: {character['0'].homeworld}</p> 
+      <Link to={`/planets/${character['0'].homeworld}`}>Homeworld: {planet.name}</Link> 
       <p>Mass: {character['0'].mass}</p> 
       <p>Eye Color: {character['0'].eye_color}</p> 
       <p>Skin Color: {character['0'].skin_color}</p> 
     </div>
+    <div>
+        <h1>Films appeared in:</h1>
+        {films.map((film) => (
+            <Link to={`/films/${film.id}`}>{film.name}</Link>
+        ))}
+    </div>
+    </>
   );
 }
 
